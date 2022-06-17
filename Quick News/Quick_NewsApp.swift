95acf92084f414b553c8426472d8a12f9dc6a15a
Chat Dep <42887325+chatsopond@users.noreply.github.com>
2022-06-17 17:26:39 +0700

    Initial Commit

---
+++ Quick News/Quick_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_NewsApp.swift
+//  Quick News
+//
+//  Created by Chatsopon Deepateep on 17/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Quick_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
